d5345fc8082d3ad0aff10ecb4b9c952667292d2f
citingdude <glenn.reumers@appwise.be>
2023-04-10 22:13:39 +0200

    Install tailwind and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import type { Component } from 'solid-js';
+import type { Component } from 'solid-js'
 
 const App: Component = () => {
   return (
     <>
       <h1>VibeFusion</h1>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
